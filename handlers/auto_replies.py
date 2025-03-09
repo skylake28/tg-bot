c25c619b16ea0efd9ff6a1cb3948e8312e3eeb2c
skylake28 <74567512+skylake28@users.noreply.github.com>
2025-03-09 09:57:00 -0700

    Update auto_replies.py

--- handlers/auto_replies.py
+++ handlers/auto_replies.py
@@ -12,8 +12,7 @@ async def set_auto_reply(message: types.Message):
         return
     trigger, response = args[1], args[2]
     auto_replies[trigger.lower()] = response
-    await message.reply(f"🤖 <b>Auto-Reply:</b>
-<i>{response}</i>", parse_mode="HTML")
+await message.reply(f"🤖 <b>Auto-Reply:</b>\n<i>{response}</i>", parse_mode="HTML")
 
 def register_handlers(dp):
     dp.include_router(router)
